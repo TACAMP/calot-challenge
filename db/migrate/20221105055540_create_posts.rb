3496924ab9c86da5eb85024a62709e4c6f39433b
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-11-05 06:25:26 +0000

    add model(Post,PostTags,Tag,Favorite,Comment,CampTool)

---
+++ db/migrate/20221105055540_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id,         null: false
+      t.text :description,        null: false
+      t.string :campsite_name
+      t.text :campsite_address
+      t.timestamps
+    end
+
+    add_index :posts, :title
+
+  end
+end
