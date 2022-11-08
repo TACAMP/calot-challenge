69e78314ffcd265b078e8bcf3eeafa5d96f3a799
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-11-08 01:52:30 +0000

    add guest_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = "ゲスト"
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
 end
