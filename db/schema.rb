742688746e962b53166c7a6b4263193cefe83430
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-11-23 08:39:22 +0000

    fix migrate2

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2022_11_06_025011) do
     t.integer "tag_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id", "tag_id"], name: "index_post_tags_on_post_id_and_tag_id", unique: true
     t.index ["post_id"], name: "index_post_tags_on_post_id"
     t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
