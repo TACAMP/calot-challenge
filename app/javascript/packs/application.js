b414e5ef111efb3c7fad2e155bfb15c56532d255
EC2 Default User <112141419+TACAMP@users.noreply.github.com>
2022-11-27 06:36:51 +0000

    [Fix] fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,12 +14,12 @@ import "bootstrap"
 import "../stylesheets/application"
 import 'cocoon'
 
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library } from "@fortawesome/fontawesome-svg-core";
-import '@fortawesome/fontawesome-free'
-library.add(fas, far, fab)
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import '@fortawesome/fontawesome-free'
+// library.add(fas, far, fab)
 
 Rails.start()
 Turbolinks.start()
